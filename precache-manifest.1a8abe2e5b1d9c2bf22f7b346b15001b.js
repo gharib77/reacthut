f978623ca62f82d3cba8c426f41eb22d0af7115c
jouali <gharibjouali@gmail.com>
2022-09-28 21:38:17 +0200

    Updates

---
+++ precache-manifest.1a8abe2e5b1d9c2bf22f7b346b15001b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c04b701f968d9b54d07b",
+    "url": "/tpreacttype/static/js/main.c04b701f.chunk.js"
+  },
+  {
+    "revision": "38a7e4af31204e3446db",
+    "url": "/tpreacttype/static/css/1.c3989e5e.chunk.css"
+  },
+  {
+    "revision": "38a7e4af31204e3446db",
+    "url": "/tpreacttype/static/js/1.38a7e4af.chunk.js"
+  },
+  {
+    "revision": "def3e79c51ecd7541d96",
+    "url": "/tpreacttype/static/js/runtime~main.def3e79c.js"
+  },
+  {
+    "revision": "d50fd7a89e8f32bba04649b8923355f9",
+    "url": "/tpreacttype/index.html"
+  }
+];
